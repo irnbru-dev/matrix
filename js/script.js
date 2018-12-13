e3ca535e72d6680106c25d96bc446fb7c3505ad0
Konovalova <darina.konovalova@it-grad.ru>
2018-12-13 18:41:03 +0300

    style, video

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Smooth Scroll to Anchors
+ */
 $('a[href^="#"]').on('click', function (e) {
     var target = $(this).attr('href');
 
@@ -5,4 +8,30 @@ $('a[href^="#"]').on('click', function (e) {
         $('html, body').animate({scrollTop: $(target).offset().top - 60}, 1000);
     }
     e.preventDefault();
+});
+
+/**
+ * Typed.js Init
+ */
+var typed = new Typed('#typed', {
+    strings: ["это то, что заставит вас остановиться и наблюдать."],
+    typeSpeed: 50,
+    showCursor: false,
+    cursorChar: '.',
+    autoInsertCss: false
+});
+
+/**
+ * Fixing contacts line
+ */
+$(function () {
+    $(window).on('scroll load', function () {
+        if ($(window).scrollTop() > $('.header').outerHeight()) {
+            $('.contacts').addClass('fixed');
+            $('#second-screen').addClass('mt90');
+        } else {
+            $('.contacts').removeClass('fixed');
+            $('#second-screen').removeClass('mt90');
+        }
+    });
 });
